ce4b20d7a5df86b627c919d3358bbbc47510ee86
Lito <nod.nole@gmail.com>
2017-09-14 19:15:20 -0300

    mensaje con los detalles del cambio

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -2,6 +2,9 @@ class UploadsController < ApplicationController
   def new
     @upload = Upload.new
   end
+  def index
+    @upload = Upload.all
+  end
 
   def create #primer metodo creado
   	@upload = Upload.create(upload_params)
@@ -10,6 +13,14 @@ class UploadsController < ApplicationController
   	else
   		render json: {message: @upload.errors.full_messages.join(',')}, :status => 400
   	end
+  end
+  def destroy
+    @upload = Upload.find(params[:id])
+    if @upload.destroy
+      redirect_to action: "index"
+    else
+      reder json: {message: @upload.errors.full_messages.join(',')}
+    end
   end
 
   private
